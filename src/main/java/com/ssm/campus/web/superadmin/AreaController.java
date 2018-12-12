c93372b9ef1582ad08457feebefa79f2a310601f
fangyir <2441430630@qq.com>
2018-12-12 17:42:06 +0800

    logback.xml相关内容

--- src/main/java/com/ssm/campus/web/superadmin/AreaController.java
+++ src/main/java/com/ssm/campus/web/superadmin/AreaController.java
@@ -2,6 +2,8 @@ package com.ssm.campus.web.superadmin;
 
 import com.ssm.campus.entity.Area;
 import com.ssm.campus.service.AreaService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,11 +23,16 @@ import java.util.Map;
 @Controller
 @RequestMapping("/superadmin")
 public class AreaController {
+   Logger logger = LoggerFactory.getLogger(AreaController.class);
     @Autowired
     private AreaService areaService;
     @RequestMapping(value = "/listarea",method = RequestMethod.GET)
     @ResponseBody
     private Map<String,Object> listArea(){
+        //日志开始信息
+        logger.info("=== start ===");
+        //获取开始时间
+        Long startTime = System.currentTimeMillis();
         Map<String,Object> modelMap = new HashMap<String, Object>();
         List<Area> list = new ArrayList<Area>();
         try {
@@ -37,6 +44,10 @@ public class AreaController {
             modelMap.put("success",false);
             modelMap.put("errMsg",e.toString());
         }
+        logger.error("test error!");
+        Long endTime = System.currentTimeMillis();
+        logger.debug("costTime[{}ms]",endTime - startTime);//花费时间
+        logger.info("=== end ===");
         return modelMap;
     }
 }
